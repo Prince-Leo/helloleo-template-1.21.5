35c93ded4d62218c85bfe70b349209077fbb4f05
LeoPrince <a061804050713@163.com>
2025-05-13 18:57:38 +0800

    添加了HelloLeoClien

---
+++ src/main/java/com/jake/helloleo/HelloLeoClient.java
@@ -0,0 +1,10 @@
+package com.jake.helloleo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloLeoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
